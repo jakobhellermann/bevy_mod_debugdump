8c9c24050e3b7a925422d2c6ef3489358643648d
Jakob Hellermann <jakob.hellermann@protonmail.com>
2021-03-21 14:32:15 +0100

    add print_render_schedule_graph example

---
+++ examples/print_render_schedule_graph.rs
@@ -0,0 +1,16 @@
+use bevy::{prelude::*, render::render_graph::RenderGraph};
+use bevy_mod_debugdump::schedule_graph_dot;
+
+fn main() {
+    App::build()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(print_render_schedule.system())
+        .run();
+}
+
+pub fn print_render_schedule(mut render_graph: ResMut<RenderGraph>) {
+    let schedule = render_graph.take_schedule().unwrap();
+    let dot = schedule_graph_dot(&schedule);
+    render_graph.set_schedule(schedule);
+    println!("{}", dot);
+}
