d4d1ca50017d5935ce432d806245987cf050f8a0
Jakob Hellermann <jakob.hellermann@protonmail.com>
2023-02-04 12:43:43 +0100

    add filtered main schedule to generate_docs

--- examples/generate_docs.rs
+++ examples/generate_docs.rs
@@ -1,6 +1,7 @@
 use std::path::PathBuf;
 
 use bevy::prelude::*;
+use bevy_mod_debugdump_stageless::Settings;
 
 fn main() -> Result<(), std::io::Error> {
     let docs_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("docs");
@@ -8,6 +9,8 @@ fn main() -> Result<(), std::io::Error> {
 
     let mut app = App::new();
     app.add_plugins(DefaultPlugins);
+
+    let settings = Settings::default();
 
     app.world
         .resource_scope::<Schedules, _>(|world, mut schedules| {
@@ -20,12 +23,28 @@ fn main() -> Result<(), std::io::Error> {
                     .build_schedule(world.components())
                     .unwrap();
 
-                let settings = bevy_mod_debugdump_stageless::Settings::default();
                 let dot =
                     bevy_mod_debugdump_stageless::schedule_to_dot(schedule, &world, &settings);
 
                 std::fs::write(docs_path.join(format!("schedule_{label:?}.dot")), dot)?;
             }
+
+            let main = schedules.get(&CoreSchedule::Main).unwrap();
+            let main_filtered_settings = Settings {
+                show_ambiguities: false,
+                include_system: Box::new(|system| {
+                    let name = system.name();
+                    name.contains("buffers")
+                }),
+                ..settings
+            };
+            let dot = bevy_mod_debugdump_stageless::schedule_to_dot(
+                main,
+                &world,
+                &main_filtered_settings,
+            );
+
+            std::fs::write(docs_path.join(format!("schedule_Main_filtered.dot")), dot)?;
 
             Ok(())
         })
