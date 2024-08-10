87a543afdfc20847c27ee3bd1098073c772d37f6
andriyDev <andriydzikh@gmail.com>
2024-08-10 14:47:00 -0700

    Rename dump-update-schedule to dump-schedule and allow dumping any schedule.
    This just looks for the schedule with the same name and dumps it instead of just the Update schedule.
    
    Sadly this does leave us to some ambiguity. We differentiate schedules by their `Debug` implementation, which is not guaranteed to be unique. However, it's very unlikely that users are going to have two of the same schedule name in the same Bevy app, so we can just ignore this.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
+use std::error::Error;
+use std::fmt::{Debug, Display};
 use std::io::Write;
 use std::{fs::File, path::PathBuf};
 
 use bevy_app::App;
+use bevy_ecs::intern::Interned;
 use bevy_ecs::schedule::{ScheduleLabel, Schedules};
 
 mod dot;
@@ -123,6 +126,74 @@ pub fn print_render_graph(app: &mut App) {
 /// TODO: Consider adding a means of selecting a schedule other than `Update`.
 pub struct CommandLineArgs;
 
+/// An error from `find_schedule`.
+enum FindScheduleError {
+    /// There was no match. Holds the requested schedule, and the list of valid
+    /// schedules by string.
+    NoMatch(String, Vec<String>),
+    MoreThanOneMatch(String),
+}
+
+impl Debug for FindScheduleError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::NoMatch(request, schedules) => {
+                f.write_fmt(format_args!("No schedules matched the requested schedule '{request}'. The valid schedules are:\n"))?;
+                for schedule in schedules {
+                    f.write_fmt(format_args!("\n{schedule}"))?;
+                }
+                Ok(())
+            }
+            Self::MoreThanOneMatch(request) => f.write_fmt(format_args!(
+                "More than one schedule matched requested schedule '{request}'"
+            )),
+        }
+    }
+}
+
+impl Display for FindScheduleError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        <Self as Debug>::fmt(self, f)
+    }
+}
+
+impl Error for FindScheduleError {}
+
+/// Looks up a schedule by its string name in `App`.
+fn find_schedule<'a>(
+    app: &'a App,
+    schedule_name: &str,
+) -> Result<Interned<dyn ScheduleLabel>, FindScheduleError> {
+    let schedules = app.world().resource::<Schedules>();
+    let schedules = schedules
+        .iter()
+        // Note we get the Interned label from `schedule` since `&dyn ScheduleLabel` doesn't `impl
+        // ScheduleLabel`.
+        .map(|(label, schedule)| (format!("{label:?}"), schedule.label()))
+        .collect::<Vec<_>>();
+
+    let mut found_label = None;
+    for (str, label) in schedules.iter() {
+        if str == schedule_name {
+            if found_label.is_some() {
+                return Err(FindScheduleError::MoreThanOneMatch(
+                    schedule_name.to_string(),
+                ));
+            }
+            found_label = Some(*label);
+        }
+    }
+
+    if let Some(label) = found_label {
+        Ok(label)
+    } else {
+        Err(FindScheduleError::NoMatch(
+            schedule_name.to_string(),
+            schedules.into_iter().map(|(str, _)| str).collect(),
+        ))
+    }
+}
+
 struct Args {
     command: ArgsCommand,
     exit: bool,
@@ -133,8 +204,13 @@ enum ArgsCommand {
     None,
     /// Dumps the render graph to the specified file path.
     DumpRender(PathBuf),
-    /// Dumps the Update schedule graph to the specified file path.
-    DumpUpdateSchedule(PathBuf),
+    /// Dumps the schedule graph.
+    DumpSchedule {
+        /// The schedule to dump.
+        schedule: String,
+        /// The path to write the graph dot to.
+        path: PathBuf,
+    },
 }
 
 fn parse_args() -> Result<Args, lexopt::Error> {
@@ -151,9 +227,10 @@ fn parse_args() -> Result<Args, lexopt::Error> {
                     return Err(arg.unexpected());
                 }
 
-                if value == "dump-update-schedule" {
+                if value == "dump-schedule" {
+                    let schedule = parser.value()?.parse()?;
                     let path = parser.value()?.parse()?;
-                    command = ArgsCommand::DumpUpdateSchedule(path);
+                    command = ArgsCommand::DumpSchedule { schedule, path };
                 } else if value == "dump-render" {
                     let path = parser.value()?.parse()?;
                     command = ArgsCommand::DumpRender(path);
@@ -165,7 +242,7 @@ fn parse_args() -> Result<Args, lexopt::Error> {
             Long("help") => {
                 println!(
                     "Commands:\n\n\
-                    dump-update-schedule <file>\n\
+                    dump-schedule <schedule_name> <file>\n\
                     dump-render <file>\n\n\
                       --no-exit Do not exit after performing debugdump actions"
                 );
@@ -190,14 +267,12 @@ fn execute_cli(app: &mut App) -> Result<Args, Box<dyn std::error::Error>> {
 
             Ok(args)
         }
-        ArgsCommand::DumpUpdateSchedule(path) => {
+        ArgsCommand::DumpSchedule { schedule, path } => {
+            let schedule = find_schedule(&app, schedule)?;
+
             let settings = schedule_graph::Settings::default();
             let mut out = File::create(path)?;
-            write!(
-                out,
-                "{}",
-                schedule_graph_dot(app, bevy_app::Update, &settings)
-            )?;
+            write!(out, "{}", schedule_graph_dot(app, schedule, &settings))?;
 
             Ok(args)
         }
